7c0a3ce0d9571995e8e2fed73f2b838aa76004b5
ArtoniG <51254087+ArtoniG@users.noreply.github.com>
2019-10-20 17:09:18 -0300

    Add files via upload

---
+++ idioma.R
@@ -0,0 +1,13 @@
+espanholBasico <- 169+12+9+8+2+4
+espanholINter<- 109+15+4+2+1+1
+espanhosemnivel <- 146+22+2+1
+francesBasico <- 36+10+1+1+6+1+1
+alemaoBasico <- 34+12+5
+francessemnivel <- 27+8+2
+espanholAvancado <- 28
+alemaosemnivel <- 25+9+2+2
+francesintermed<-25
+japonesbasico <- 27
+ingles <- 15+2+1
+francesavan <- 14
+italiano <- 18
